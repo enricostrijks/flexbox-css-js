c45655cc37201b2fe5ba2e86716d1f683d073dc2
Enrico <strijksenrico@gmail.com>
2020-12-04 12:55:47 +0100

    Stap 6

--- animaties.js
+++ animaties.js
@@ -1,3 +1,17 @@
 document.querySelector(".animeer-knop").addEventListener("click", (e) => {
   document.querySelector(".vlakje").classList.toggle("vlakje__naar-rechts");
 });
+
+setTimeout(() => {
+  document.querySelector(".vlakje").classList.toggle("vlakje__naar-rechts");
+}, 2000);
+
+const menuKnop = document.querySelector(".nav__knop");
+const navigatieItems = document.querySelectorAll(".nav__link");
+menuKnop.addEventListener("click", () => {
+  navigatieItems.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.toggle("nav__link--schuif-in");
+    }, 30 * index);
+  });
+});
